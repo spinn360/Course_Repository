bd3fc0fe96a3e2c5e0093a4aeb9d1fcc8c1aea84
Shawn <smcclu7@wgu.edu>
2026-06-25 12:18:30 -0700

    added P2 and just a few lines of code with comments

---
+++ Lesson1/P2.py
@@ -0,0 +1,4 @@
+#Here I will be moving on from p1 where I ended with user input so the beginning will continue with input
+print('hello, and welcome back are you ready to continue?', end= ' ')
+cont = input()
+print('You said', cont, 'that is either great or not I have no idea LOL')
