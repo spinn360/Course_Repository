94c94ef81c16bd8c9839a7460a3dbdf764317909
Shawn <smcclu7@wgu.edu>
2026-06-25 11:33:48 -0700

    p1.py testing variables and print statements

--- Lesson1/P1.py
+++ Lesson1/P1.py
@@ -5,3 +5,16 @@ artist = '360'
 albumn = 5
 
 print('There is a new',artist,'cd called',cd, 'and it is number',albumn)
+print(cd,type(cd))
+print(artist,type(artist))
+print(albumn,type(albumn))
+cd:str = 'rap'
+artist:str = '360'
+albumn:int = 5
+
+print('There is a new',artist,'cd called',cd, 'and it is number',albumn)
+print(cd,type(cd))
+print(artist,type(artist))
+print(albumn,type(albumn))
+
+print(int(artist) + albumn)
