b7526b6f8f3a88f47579b30cf24ed413c5aa3398
Shawn <smcclu7@wgu.edu>
2026-06-25 21:01:51 -0700

    Added dog year calculator

--- Lesson1/P2.py
+++ Lesson1/P2.py
@@ -4,8 +4,18 @@ cont = input()
 print('You said', cont, 'that is either great or not I have no idea LOL')
 
 # ok
+# salary calculator
 hours = 40
 weeks = 52
 hourly_wage = int(input('Enter Hourly wage: '))
 
 print('Salary is', hourly_wage * hours * weeks)
+
+# dog year calculator
+human_years = int(input('Enter age of dog (in human years): '))
+print()
+
+dog_years = 7 * human_years
+
+print(human_years, 'human years is about', end=' ')
+print(dog_years, 'dog years.')
