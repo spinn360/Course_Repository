ee626380db639b99bd857959dd20c3a9561db5a5
Shawn <smcclu7@wgu.edu>
2026-06-25 20:58:46 -0700

    Added yearly Salary calculator

--- Lesson1/P2.py
+++ Lesson1/P2.py
@@ -2,3 +2,10 @@
 print('hello, and welcome back are you ready to continue?', end= ' ')
 cont = input()
 print('You said', cont, 'that is either great or not I have no idea LOL')
+
+# ok
+hours = 40
+weeks = 52
+hourly_wage = int(input('Enter Hourly wage: '))
+
+print('Salary is', hourly_wage * hours * weeks)
