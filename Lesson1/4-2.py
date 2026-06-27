44f9994c25eaa6ee8ac1efc119e7a9c3e1339d06
Shawn <smcclu7@wgu.edu>
2026-06-27 12:44:43 -0700

    Modified lesson 4-2

--- Lesson1/4-2.py
+++ Lesson1/4-2.py
@@ -1,15 +1,15 @@
 
-#single and double quotes are treated equal
+#  single and double quotes are treated equal
 my_str_1 = 'Hello'
 my_str_2 = "World"
 
 print(my_str_1,my_str_2)
 
-#fstring to print out Hello World
+# fstring to print out Hello World
 print(f"{my_str_1} {my_str_2}")
 
 
-#Triple quote to create multiline strings
+# Triple quote to create multiline strings
 my_str_3 = """Multiline
 string"""
 my_str_4 = '''Another
@@ -19,8 +19,30 @@ print(my_str_3)
 print("  ")
 print(my_str_4)
 
-#creating strings with quotes in them use opposing quotes to wrap
+# creating strings with quotes in them use opposing quotes to wrap or use \
 msg = "It's a sunny day"
 quote = 'She said, "Hello World!"'
-print(msg, "\n", quote) #produces a space from comma after new line
-print(msg, "\n" + quote) #does not produce space from + after new line
+print(msg, "\n", quote) # produces a space from comma after new line
+print(msg, "\n" + quote) # does not produce space from + after new line
+msg = 'It\'s a sunny day'
+quote = "She said, \"Hello!\""
+print(msg, "\n" + quote) 
+
+# searching in a string
+my_str = 'Hello world'
+
+print('Hello' in my_str)  # True
+print('hey' in my_str)    # False
+print('hi' in my_str)    # False
+print('e' in my_str)  # True
+print('f' in my_str)  # False
+
+#getting length using len() fuction
+
+
+print(len(my_str))  # returns 11
+
+# get specific letters from index of string 
+
+print(my_str[0])  # H
+print(my_str[6])  # w
