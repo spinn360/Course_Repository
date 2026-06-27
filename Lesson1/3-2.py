57fbb01b054fbccfdb1f76ba2248e30c2e0ef054
Shawn <smcclu7@wgu.edu>
2026-06-27 15:28:34 -0700

    Updated 3-2

---
+++ Lesson1/3-2.py
@@ -0,0 +1,12 @@
+
+#luggage calculator
+luggage_weight = 10
+backpack_weight = 10
+suitcase_weight = 10
+
+
+#luggage_weight = int(input())
+#backpack_weight = int(input())
+#suitcase_weight = int(input())
+
+print(luggage_weight+backpack_weight+suitcase_weight)
