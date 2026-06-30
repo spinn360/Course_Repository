8c5dd87ba4844de3c62ee4be10e41f729437231c
Shawn <smcclu7@wgu.edu>
2026-06-30 05:17:36 -0700

    constants.py earth and mars gravitational pull

---
+++ Lesson1/constants.py
@@ -0,0 +1,8 @@
+# Gravitational constants for various planets
+
+earth_g = 9.81  # m/s^2
+mars_g = 3.71
+
+if __name__ == '__main__':
+    print(f'Earth constant: {earth_g:.2f}')
+    print(f'Mars constant: {mars_g:.2f}')
