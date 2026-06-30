dd539269233d2ad6f9387c822abbd9ab2b3f6acb
Shawn <smcclu7@wgu.edu>
2026-06-30 11:24:02 -0700

    modified 6-5 adding more to it

--- Lesson1/6-5.py
+++ Lesson1/6-5.py
@@ -16,3 +16,20 @@ num5 = int(input('Enter 5 digit number: '))
 result = num5 * 100001
 
 print(result)
+
+
+num = int(input('Enter 2 digit number: '))
+
+result = num * (3 * 7 * 13 * 37)
+
+print(result)
+num3 = int(input('Enter 3 digit number: '))
+
+result = num3 * (7 * 11 * 13)
+
+print(result)
+num5 = int(input('Enter 5 digit number: '))
+
+result = num5 * (11 * 9091)
+
+print(result)
