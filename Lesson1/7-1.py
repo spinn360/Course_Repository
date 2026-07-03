64c787b0776dd23a886659985a271851497f885c
Shawn <smcclu7@wgu.edu>
2026-07-03 09:11:47 -0700

    adding more study examples to python

---
+++ Lesson1/7-1.py
@@ -0,0 +1,13 @@
+exam1_grade = float(input("Enter score on Exam 1 (out of 50): "))
+exam2_grade = float(input("Enter score on Exam 2 (out of 50): "))
+exam3_grade = float(input("Enter score on Exam 3 (out of 50): "))
+exam4_grade = float(input('Enter score on Exam 4 (out of 50): '))
+exam1_grade /= 50 * 100
+exam2_grade /= 50 * 100
+exam3_grade /= 50 * 100
+exam4_grade /= 50 * 100
+
+overall_grade = (exam1_grade + exam2_grade + exam3_grade + exam4_grade) / 4 *10000
+
+print(f"Your overall grade is: {overall_grade}")
+
