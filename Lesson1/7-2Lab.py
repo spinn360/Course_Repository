8a4e0c767cb5951bde2b81a4e22b153bb54afc26
Shawn <smcclu7@wgu.edu>
2026-07-03 12:16:50 -0700

    added labs

---
+++ Lesson1/7-2Lab.py
@@ -0,0 +1,21 @@
+user_age_years = int(input('Enter your age in years:\n'))
+
+user_age_days = user_age_years  * 365
+
+print(f'You are at least {user_age_days} days old.')
+'''Create a different version of the program that:
+
+Calculates the user's age in minutes and seconds.
+Estimates the approximate number of times the user's 
+heart has beaten in his/her lifetime using an average 
+heart rate of 72 beats per minute.
+Estimates the number of times the person has sneezed in his/her lifetime.
+Estimates the number of calories that the person has expended in his/her 
+lifetime (research on the internet to obtain a daily estimate). Also 
+calculate the number of sandwiches (or other common food item) that 
+equals that number of calories.
+Be creative: Pick several interesting health-related statistics. 
+Try searching the internet to determine how to calculate that data, 
+and create a program to perform that calculation. 
+The program can ask the user to enter any information needed to perform 
+the calculation.'''
