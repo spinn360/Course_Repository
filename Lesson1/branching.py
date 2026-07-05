6ca1a7a29842cb79116528ee88cc5ea5ee6183f0
Shawn <smcclu7@wgu.edu>
2026-07-05 16:12:24 -0700

    if elif else statement for restaraunt table assignment logic statement

---
+++ Lesson1/branching.py
@@ -0,0 +1,13 @@
+# logical if statements for assigning table size to group size at a restaraunt
+
+party_size = int(input('How many? '))
+if party_size > 1 and party_size < 3 :
+    print('you get a small table')
+elif party_size >=3 and party_size < 10:
+    print('You get a large table')
+elif party_size >= 10 and party_size < 25:
+    print('We will move two large tables together for you')
+elif party_size >= 25:
+    print('Looks like we will be booking you an entire room')
+else:
+    print('Counter seating for you')
