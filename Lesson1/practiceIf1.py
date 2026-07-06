213f23ed61d0e375e72622dd4c2e09a3da037fc9
Shawn <smcclu7@wgu.edu>
2026-07-05 18:49:07 -0700

    updated the if statement in practice if 1 just to see how many spaces after a branch is correct since vs code auto populates indent 4

--- Lesson1/practiceIf1.py
+++ Lesson1/practiceIf1.py
@@ -4,6 +4,6 @@ div_remainder = user_num %2
 if div_remainder == 0:
     print(f'{user_num} is even.')
 elif div_remainder == 1000:
-    print('this is impossible')
+    print('this is impossible') # 4 spaces
 else:
     print(f'{user_num} is odd.')
