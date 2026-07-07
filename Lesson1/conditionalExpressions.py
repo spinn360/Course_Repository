4e689baee8cbcf7819f195ea6087c3a36622e9f4
Shawn <smcclu7@wgu.edu>
2026-07-07 09:13:55 -0700

    example of conditional expression conditionalExpressions.py

---
+++ Lesson1/conditionalExpressions.py
@@ -0,0 +1,6 @@
+x = int(input('Enter whole number: ')) 
+y = 5 if (x == 2) else 9 * x      # conditional expression here
+if x == 2:
+    print(f'You entered {x} which makes the output {y} always')
+else:
+    print(f'you entered {x} which times 9 = {y}')
