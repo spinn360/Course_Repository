aa86614806c0d048f01b23b48a8b40adf0d04fd3
Shawn <smcclu7@wgu.edu>
2026-07-07 09:33:19 -0700

    added 2nd example to conditionalExpressions.py

--- Lesson1/conditionalExpressions.py
+++ Lesson1/conditionalExpressions.py
@@ -4,3 +4,11 @@ if x == 2:
     print(f'You entered {x} which makes the output {y} always')
 else:
     print(f'you entered {x} which times 9 = {y}')
+
+
+num_users = int(input('Enter num users: '))
+update_direction = int(input('Enter num 1-4: '))
+
+num_users = num_users + 1 if (update_direction == 3) else num_users - 1  # conditional expression
+
+print(f'New value is: {num_users}')
