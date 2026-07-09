712486141815952cbb4c4d7bdd3424827f26e2e5
Shawn <spinn360@gmail.com>
2026-07-08 20:33:02 -0700

    my first for loop that sums numbers from a list called carrot

---
+++ Lesson1/forloops1.py
@@ -0,0 +1,18 @@
+carrot = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    10,
+    20,
+    30,
+    40,
+    50
+]
+
+total = 0
+for num in carrot:
+    total += num
+    print(f'Current total so far is {total}')
+print(total)
