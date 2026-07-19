ba373129796f75f4b4bcbf179272a0e37a600075
Shawn <smcclu7@wgu.edu>
2026-07-19 12:59:47 -0700

    added note to skylight_cave.py

--- Lesson1/skylight_cave.py
+++ Lesson1/skylight_cave.py
@@ -8,8 +8,8 @@ cave_wall = [
 ]
 
 # Write your nested for loops using range() here!
-for r in range(cave_size):
-    for c in range(cave_size):
+for r in range(cave_size): #r row
+    for c in range(cave_size): #c column
         if r < c:
             cave_wall[r][c] = '*'
 
