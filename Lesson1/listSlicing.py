8c3df12b907801c95808fb2904e65e7bca19206f
Shawn <smcclu7@wgu.edu>
2026-07-19 12:57:26 -0700

    list slicing practes added

---
+++ Lesson1/listSlicing.py
@@ -0,0 +1,10 @@
+mylist = [5, 10, 20, 30, 40, 50]
+print(mylist[0:2])
+print(mylist[0:-1:2])
+print(mylist[0:])
+print(mylist[3:])
+print(mylist[:-1])
+print(mylist[:4])
+mylistcopy = mylist[:]
+print(mylistcopy)
+print(mylistcopy == mylist)
