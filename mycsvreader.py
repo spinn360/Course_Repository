c9a48c99e1aa6ce7b9f232f81c188e6f0217458d
Shawn <smcclu7@wgu.edu>
2026-07-26 20:15:04 -0700

    csv reading exercises in mycsv.csv mycsvreader.py mycsvreadercalc.py

---
+++ mycsvreader.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('mycsv.csv', 'r') as csvfile:
+    grades_reader = csv.reader(csvfile, delimiter=',')
+
+    row_num = 1
+    for row in grades_reader:
+        print(f'Row #{row_num}: {row}')
+        row_num +=1
