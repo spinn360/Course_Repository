344264ab763accdf588b34494bf7f6cc2d3e6aab
Shawn <smcclu7@wgu.edu>
2026-07-29 09:10:20 -0700

    updated whileloops4.py

--- Lessons/whileloops4.py
+++ Lessons/whileloops4.py
@@ -1,5 +1,5 @@
 result = 0
-num_input = int(input())
+num_input = int(input('enter any positive number or (enter -1 to quit)'))
 
 while num_input >=0:
     if num_input  % 4 == 0:
@@ -9,4 +9,4 @@ while num_input >=0:
         result += 1
     num_input = int(input())
 
-print(f'Result is {result}')
+print(f'Result of wins is {result}')
